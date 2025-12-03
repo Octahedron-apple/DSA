dacf988faf3d0266d39368bb86b528a28b9b3628
Octahedron-apple <bhavya1singh1@gmail.com>
2025-12-03 23:53:19 +0530

    Implement delete operations for linked list
    Add methods to delete nodes from the linked list.

--- python/Linked List/obj.py
+++ python/Linked List/obj.py
@@ -54,3 +54,54 @@ class LinkedList:
             print(current_node.data, end=" -> ")
             current_node = current_node.next
         print("end")
+    def delete_first(self):
+        
+        if self.head is None:
+            print("empty")
+            return
+
+       
+        self.head = self.head.next
+    def delete_last(self):
+        
+        if self.head is None:
+            print("List is empty")
+            return
+
+        
+        if self.head.next is None:
+            self.head = None
+            return
+
+        
+        second_last = self.head
+        while second_last.next.next:
+            second_last = second_last.next
+
+        
+        second_last.next = None
+        def delete_at_index(self, index):
+
+            if self.head is None:
+                print("List is empty")
+                return
+
+            if index == 0:
+                self.head = self.head.next
+                return
+
+            current = self.head
+            position = 0
+
+
+            while current is not None and position < index - 1:
+                current = current.next
+                position += 1
+
+          
+            if current is None or current.next is None:
+                print("Index out of bounds")
+                return
+
+
+            current.next = current.next.next
