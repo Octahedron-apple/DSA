2d8e32c300cfb0b53abbd45917da26cbd7f96fdc
Octahedron-apple <bhavya1singh1@gmail.com>
2025-12-05 17:15:02 +0530

    Clean up blank lines in obj.h
    Removed unnecessary blank lines in obj.h

--- c/Linked List/obj.h
+++ c/Linked List/obj.h
@@ -1,28 +1,14 @@
 #ifndef OBJ_H
 #define OBJ_H
-
-
 #include <stdio.h>
-
-
 typedef struct Node {
     int data;               
     struct Node* next;      
 } Node;
-
-
 Node* create_node(int data);
-
 void insert_at_head(Node** head, int data);
-
 void insert_at_tail(Node** head, int data);
-
-
 void delete_node(Node** head, int key);
-
-
 void print_list(Node* head);
-
 void free_list(Node* head);
-
 #endif 
